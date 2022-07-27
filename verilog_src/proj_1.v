9e728cc327e1dc36a85ff4ff3d9a894d40704201
Lindsay <ki6lzn@gmail.com>
2022-07-27 17:43:23 -0400

    bug fix on lab 1

--- verilog_src/proj_1.v
+++ verilog_src/proj_1.v
@@ -2,20 +2,20 @@
 // goal: turn on LEDs when switches are pushed
 // 07/26/2022
 
-module proj_1 (
-    input  i_switch_0,
-    input  i_switch_1,`
-    input  i_switch_2,
-    input  i_switch_3,
-    output o_led_0   ,
-    output o_led_1   ,
-    output o_led_2   ,
-    output o_led_3 );
+module proj_1 (    
+    input  i_Switch_1,
+    input  i_Switch_2,
+    input  i_Switch_3, 
+    input  i_Switch_4, 
+    output o_LED_1   ,
+    output o_LED_2   ,
+    output o_LED_3   ,
+    output o_LED_4 );
 
-    assign o_led_0 = i_switch_0;
-    assign o_led_1 = i_switch_1;
-    assign o_led_2 = i_switch_2;
-    assign o_led_3 = i_switch_3;
+    assign o_LED_1 = i_Switch_1;
+    assign o_LED_2 = i_Switch_2;
+    assign o_LED_3 = i_Switch_3;
+    assign o_LED_4 = i_Switch_4;
 
 
 endmodule
