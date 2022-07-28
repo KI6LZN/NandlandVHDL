ad2b2cd0aeb74670f2e95b9947df89018b02b463
Lindsay White <ki6lzn@gmail.com>
2022-07-27 17:10:02 -0700

    update debouncer

--- verilog_src/debounce_module.v
+++ verilog_src/debounce_module.v
@@ -9,28 +9,23 @@ module debounce_module (
     parameter COUNTER_LIMIT = 250000; // 10ns at 25MHz clock
 
     reg [17:0] counter_i = 0;
-    reg count_flag_i     = 1'b0;
     reg switch_i         = 1'b0;
 
 
     always @ (posedge i_clk)
     begin
-      if (switch_i !== i_switch)
-        count_flag_i <= 1'b1;
-
-      if (count_flag_i == 1'b1 && counter_i < COUNTER_LIMIT)
-        counter_i <= counter_i + 1;
-
-      else if (count_flag_i == 1'b1 && counter_i == COUNTER_LIMIT)
+      if (switch_i !== i_switch && counter_i < COUNTER_LIMIT)
         begin
-          count_flag_i <= 1'b0;
+        counter_i <= counter_i + 1;
+        end
+      else if (counter_i == COUNTER_LIMIT)
+        begin
           counter_i    <= 0;
           switch_i     <= i_switch;
         end
 
       else
         begin
-          count_flag_i <= 1'b0;
           counter_i    <= 0;
         end
 
