d91c72aa31bbf0ab054c3692e8549b59563e081f
Lindsay White <ki6lzn@gmail.com>
2022-07-28 12:02:57 -0700

    Implement two 7-segment LEDs and ability to count both directions

--- verilog_src/proj_5.v
+++ verilog_src/proj_5.v
@@ -5,6 +5,14 @@
 module proj_5 (
     input  i_Clk       ,
     input  i_Switch_1  ,
+    input  i_Switch_2  ,
+    output o_Segment1_A,
+    output o_Segment1_B,
+    output o_Segment1_C,
+    output o_Segment1_D,
+    output o_Segment1_E,
+    output o_Segment1_F,
+    output o_Segment1_G,
     output o_Segment2_A,
     output o_Segment2_B,
     output o_Segment2_C,
@@ -13,22 +21,41 @@ module proj_5 (
     output o_Segment2_F,
     output o_Segment2_G  );
 
-    parameter COUNTER_LIMIT = 16;
+    parameter LED_LIMIT = 9;
 
     // signals
-    reg switch_i = 1'b0;
-    reg [3:0] counter_i = 0;
+    reg r_switch_i = 1'b0;
+    reg l_switch_i = 1'b0;
+    reg [3:0] r_counter_i = 0;
+    reg [3:0] l_counter_i = 0;
 
-    wire switch_w;
+    wire switch_l_w;
+    wire switch_r_w;
 
     debounce_module Debouncing
     (.i_clk(i_Clk)        ,
      .i_switch(i_Switch_1),
-     .o_switch(switch_w)  );
+     .o_switch(switch_l_w)  );
+
+    debounce_module Debouncing
+    (.i_clk(i_Clk)        ,
+     .i_switch(i_Switch_2),
+     .o_switch(switch_r_w)  );
+
+    seven_segment Left
+    (.i_clk(i_Clk),
+     .i_value(l_counter_i),
+     .o_seg_A(o_Segment1_A),
+     .o_seg_B(o_Segment1_B),
+     .o_seg_C(o_Segment1_C),
+     .o_seg_D(o_Segment1_D),
+     .o_seg_E(o_Segment1_E),
+     .o_seg_F(o_Segment1_F),
+     .o_seg_G(o_Segment1_G));
 
      seven_segment Right
      (.i_clk(i_Clk),
-      .i_value(counter_i),
+      .i_value(r_counter_i),
       .o_seg_A(o_Segment2_A),
       .o_seg_B(o_Segment2_B),
       .o_seg_C(o_Segment2_C),
@@ -40,12 +67,51 @@ module proj_5 (
 
     always @ (posedge i_Clk)
       begin
-        switch_i <= switch_w;
-        if (switch_i == 1'b0 && switch_w == 1'b1 && counter_i < COUNTER_LIMIT)
-          counter_i <= counter_i + 1;
-        else if (switch_i == 1'b0 && switch_w == 1'b1 && counter_i == COUNTER_LIMIT)
-          counter_i <= 0;
-      end
+        r_switch_i <= switch_r_w;
+        l_switch_i <= switch_l_w;
+
+        // if right switch increments and less than 10, increment counter
+        if (r_switch_i == 1'b0 && switch_r_w == 1'b1 && r_counter_i <= LED_LIMIT)
+          r_counter_i <= r_counter_i + 1;
+
+        // if right switch increments and counter is at counter limit, reset right counter and increment left
+        if (r_switch_i == 1'b0 && switch_r_w == 1'b1 && r_counter_i == LED_LIMIT)
+          begin
+            // if left counter is at the end, reset both counters
+            if (l_counter_i == LED_LIMIT)
+              begin
+                l_counter_i <= 0;
+                r_counter_i <= 0;
+              end
+            // otherwise increment left and reset right
+            else
+              begin
+                l_counter_i <= l_counter_i + 1;
+                r_counter_i <= 0;
+              end
+          end
+
+        // if left switch increments and greater than 0, decrement counter
+        if (l_switch_i == 1'b0 && switch_l_w == 1'b1 && r_counter_i > 0)
+          r_counter_i <= r_counter_i - 1;
+
+        // if right switch increments and counter is at counter limit, reset right counter and increment left
+        if (l_switch_i == 1'b0 && switch_l_w == 1'b1 && r_counter_i == 0)
+          begin
+            // if left counter is at the end, reset both counters
+            if (l_counter_i == 0)
+              begin
+                l_counter_i <= LED_LIMIT;
+                r_counter_i <= LED_LIMIT;
+              end
+            // otherwise decrement left and reset right
+            else
+              begin
+                l_counter_i <= l_counter_i - 1;
+                r_counter_i <= LED_LIMIT;
+              end
+          end
+
 
 
 endmodule
